558710751a470dbd419f0aeb98307098ab95e5ee
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-15 10:20:28 -0700

    01 Building an API - Intro to Flask, Setup, and Read completed

---
+++ app/routes.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, jsonify
+from .models.book import books
+
+hello_world_bp = Blueprint("hello_world", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@hello_world_bp.route("/hello-world", methods=["Get"])
+def say_hello_world():
+    my_beautiful_response_body = "Hello, World!"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]}
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
+
