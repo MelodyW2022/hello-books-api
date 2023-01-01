d1909bd1232ba4c13f203be4d5799543c1b0cb7b
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-31 18:16:21 -0700

    feat: Create read one book end point

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,7 @@
 from flask import Blueprint, jsonify
 from .models.book import books
 
-hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books")
-
-@hello_world_bp.route("/hello-world", methods=["Get"])
-def say_hello_world():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]}
-
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
 
 @books_bp.route("", methods=["GET"])
 def handle_books():
@@ -37,5 +13,17 @@ def handle_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>",methods = ["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        
 
 
