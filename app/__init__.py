a5e5a31cf88cb1b3a862d4251e95aff7a6dc4048
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-01-01 22:25:01 -0700

    feat: Set up models

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
-
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     # create Flask applicaton object
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development"
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.book import Book
 
     from .routes import books_bp
     app.register_blueprint(books_bp)
--- app/models/book.py
+++ app/models/book.py
@@ -1,11 +1,18 @@
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-] 
+# books = [
+#     Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ] 
+
+from app import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
